0e05648ecbbf1b3e3c53fbffbf9d74887da6a42e
Dmitriy Shukaliuk <mityaua@gmail.com>
2021-06-03 13:11:20 +0300

    Fixed async & path

--- contacts.js
+++ contacts.js
@@ -4,74 +4,66 @@ const path = require("path");
 const { customAlphabet } = require("nanoid");
 const newId = customAlphabet("1234567890", 10);
 
-const contactsPath = path.resolve("./db/contacts.json");
+const contactsPath = path.join("./db/contacts.json");
 
 // Получаем и выводим весь список контактов в виде таблицы
-function listContacts() {
-  (async () => {
-    try {
-      const data = await fs.readFile(contactsPath, "utf8");
+async function listContacts() {
+  try {
+    const data = await fs.readFile(contactsPath, "utf8");
 
-      return console.table(JSON.parse(data));
-    } catch (error) {
-      console.error(error.message);
-    }
-  })();
+    return console.table(JSON.parse(data));
+  } catch (error) {
+    console.error(error.message);
+  }
 }
 
 // Получаем контакт по id
-function getContactById(contactId) {
-  (async () => {
-    try {
-      const data = await fs.readFile(contactsPath, "utf8");
-      const contacts = JSON.parse(data);
+async function getContactById(contactId) {
+  try {
+    const data = await fs.readFile(contactsPath, "utf8");
+    const contacts = JSON.parse(data);
 
-      const contact = contacts.filter((contact) => contact.id === Number(contactId));
+    const contact = contacts.filter((contact) => contact.id === Number(contactId));
 
-      return console.log(contact);
-    } catch (error) {
-      console.error(error.message);
-    }
-  })();
+    return console.log(contact);
+  } catch (error) {
+    console.error(error.message);
+  }
 }
 
 // Удаляем контакт
-function removeContact(contactId) {
-  (async () => {
-    try {
-      const data = await fs.readFile(contactsPath, "utf8");
-      const contacts = JSON.parse(data);
+async function removeContact(contactId) {
+  try {
+    const data = await fs.readFile(contactsPath, "utf8");
+    const contacts = JSON.parse(data);
 
-      const filteredContacts = contacts.filter((contact) => contact.id != contactId);
+    const filteredContacts = contacts.filter((contact) => contact.id != contactId);
 
-      await fs.writeFile(contactsPath, JSON.stringify(filteredContacts, null, 2));
-    } catch (error) {
-      console.error(error.message);
-    }
-  })();
+    await fs.writeFile(contactsPath, JSON.stringify(filteredContacts, null, 2));
+  } catch (error) {
+    console.error(error.message);
+  }
 }
 
 // Добавялем контакт
-function addContact(name, email, phone) {
-  (async () => {
-    const contact = {
-      id: Number(newId()),
-      name,
-      email,
-      phone,
-    };
+async function addContact(name, email, phone) {
+  const contact = {
+    id: Number(newId()),
+    name,
+    email,
+    phone,
+  };
 
-    try {
-      const data = await fs.readFile(contactsPath, "utf8");
-      const contacts = JSON.parse(data);
+  try {
+    const data = await fs.readFile(contactsPath, "utf8");
+    const contacts = JSON.parse(data);
 
-      contacts.push(contact); // Нужны доп проверки при добавлении?
+    contacts.push(contact); // Нужны доп проверки при добавлении?
 
-      await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
-    } catch (error) {
-      console.error(error.message);
-    }
-  })();
+    await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  } catch (error) {
+    console.error(error.message);
+  }
 }
 
 module.exports = {
